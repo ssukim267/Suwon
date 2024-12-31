bd894d85d6099f7a29cfbd271190f1b67f1cb368
kujieun <115772471+kujieun@users.noreply.github.com>
2024-12-31 22:31:29 +0900

    최종
    - 음원으로 대체 (대신 NaverTTS.cs 삭제X 필요시 사용)
    - 오브젝트 비활성화 안되는 문제 해결
    - 오브젝트 비활성화 안되는 문제 해결
    - 설명 & 음원 재사용 안되는 문제 해결

--- Assets/Scripts/AR/ARwithAudio.cs
+++ Assets/Scripts/AR/ARwithAudio.cs
@@ -5,40 +5,27 @@ using UnityEngine.XR.ARFoundation;
 
 public class ARwithAudio : MonoBehaviour
 {
-    // ARTrackedImageManager를 통해 AR 이미지 추적 관리
     public ARTrackedImageManager trackedImageManager;
 
     // 3D 오브젝트 리스트
     public List<GameObject> _objectList = new List<GameObject>();
 
-    // 오브젝트에 대한 설명 텍스트
-    public TextMeshProUGUI objectDescriptionText;
-
-    // 오브젝트와 음원 파일을 연결할 딕셔너리
-    public Dictionary<string, AudioClip> audioDic = new Dictionary<string, AudioClip>();
-
-    // AudioSource 컴포넌트
-    private AudioSource audioSource;
-
     // 이미지 이름을 키로, 해당 이미지를 추적할 때 표시할 3D 오브젝트를 값으로 가지는 딕셔너리
     private Dictionary<string, GameObject> _prefabDic = new Dictionary<string, GameObject>();
 
-    // 설명 텍스트를 미리 설정
-    private Dictionary<string, string> _objectDescriptions = new Dictionary<string, string>()
-    {
-        {"gyojeon", "왜검교전은 일본에서 도입된 칼류의 도검무예이다. 두 사람이 마주 보고 칼을 사용하여 교전하는 형식으로 공방의 자세를 취하는 것이 특징이다."},
-        {"deungpae", "등패는 중국에서 도입된 칼류의 도검무예이다. 등패와 함께 짧은 칼인 요도와 표창으로 무장하여, 먼저 표창을 던지고 이후 등패와 칼로 적을 제압하는 근접전을 펼친다."},
-        {"ssanggeom", "쌍검은 중국에서 도입된 칼류의 도검무예이다. 두 개의 칼을 사용하는 기예로서 하나로는 막고 다른 칼로 상대를 베거나 찌른다."},
-        {"ssangsudo", "쌍수도는 일본에서 도입된 칼류의 도검무예이다. 모든 검법의 기본으로서 검의 운용에 필요한 발도술, 베기, 막기, 찌르기, 착검술, 안법, 보법 등으로 이루어져 있다."},
-        {"nangseon", "낭선은 중국에서 도입된 창류의 단병무예이다. 낭선은 대나무 가지에 작은 쇠 날을 9개에서 11개를 달아 만든 무기로 찌르는 형태의 자법을 사용하고 있다."},
-        {"dangpa", "당파는 중국에서 도입된 창류의 무기이다. 정봉이라고 불리는 가운데의 긴 날과 양쪽으로 두 개의 가지가 뻗은 모양을 갖고 있다."},
-        {"woldo", "월도는 중국에서 도입된 칼류의 도검무예이다. 도의 형태로 찍어 베는 감법을 사용하고 있다."},
-        {"hyeopdo", "협도는 중국에서 도입된 칼류의 도검무예이다. 장검처럼 눈썹 모양을 하고 도를 이용하여 찍어 베는 형태의 감법을 사용하고 있다."},
-        {"gwonbeop", "권법은 손과 발을 이용하여 상대방을 공격하거나 방어하는 맨손 무예이다. 무예를 배우는 자가 가장 먼저 익히는 기초적인 무예로서 주먹으로 치는 형태의 격법을 사용하고 있다."},
-    };
+    // 이미지 이름과 해당 패널 오브젝트를 매핑하는 딕셔너리
+    private Dictionary<string, GameObject> imageToPanelMap = new Dictionary<string, GameObject>();
 
-    // 오브젝트를 담고 있는 부모 오브젝트
     public GameObject ObjectPool;
+    public GameObject panelDangpa;    // "danpa" 패널
+    public GameObject panelDeungpae; // "deungpae" 패널
+    public GameObject panelGyojeon;  // "gyojeon" 패널
+    public GameObject panelSsanggeom; // "ssanggeom" 패널
+    public GameObject panelSsangsudo; // "ssangsudo" 패널
+    public GameObject panelNangseon;  // "nangseon" 패널
+    public GameObject panelWoldo;     // "woldo" 패널
+    public GameObject panelHyeopdo;   // "hyeopdo" 패널
+    public GameObject panelGwonbeop;  // "gwonbeop" 패널
 
     // 오브젝트 확대 및 축소
     private float previousTouchDistance = 0;
@@ -50,25 +37,33 @@ public class ARwithAudio : MonoBehaviour
 
     void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
-
         // 오브젝트 리스트에 있는 3D 오브젝트를 이름과 함께 딕셔너리에 추가
         foreach (GameObject obj in _objectList)
         {
             string tName = obj.name;
             _prefabDic.Add(tName, obj); // 오브젝트 이름을 키로 딕셔너리에 저장
         }
+
+
+        // 패널 오브젝트를 딕셔너리에 매핑
+        imageToPanelMap.Add("dangpa", panelDangpa);
+        imageToPanelMap.Add("deungpae", panelDeungpae);
+        imageToPanelMap.Add("gyojeon", panelGyojeon);
+        imageToPanelMap.Add("ssanggeom", panelSsanggeom);
+        imageToPanelMap.Add("ssangsudo", panelSsangsudo);
+        imageToPanelMap.Add("nangseon", panelNangseon);
+        imageToPanelMap.Add("woldo", panelWoldo);
+        imageToPanelMap.Add("hyeopdo", panelHyeopdo);
+        imageToPanelMap.Add("gwonbeop", panelGwonbeop);
     }
 
     private void OnEnable()
     {
-        // 이미지가 추적되거나 업데이트될 때 호출되는 이벤트 핸들러 등록
         trackedImageManager.trackedImagesChanged += ImageChanged;
     }
 
     private void OnDisable()
     {
-        // 컴포넌트가 비활성화되면 이벤트 핸들러 해제
         trackedImageManager.trackedImagesChanged -= ImageChanged;
     }
 
@@ -125,9 +120,6 @@ public class ARwithAudio : MonoBehaviour
         }
     }
 
-    // 이미 재생된 이미지 이름을 추적하기 위한 HashSet
-    private HashSet<string> playedAudioSet = new HashSet<string>();
-
     private void ImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
         foreach (ARTrackedImage trackedImage in eventArgs.added)
@@ -135,13 +127,6 @@ public class ARwithAudio : MonoBehaviour
             if (trackedImage != null)
             {
                 UpdateImage(trackedImage);
-
-                // 처음 인식된 경우에만 재생
-                if (!playedAudioSet.Contains(trackedImage.referenceImage.name))
-                {
-                    PlayAudio(trackedImage.referenceImage.name);
-                    playedAudioSet.Add(trackedImage.referenceImage.name);
-                }
             }
         }
 
@@ -151,7 +136,10 @@ public class ARwithAudio : MonoBehaviour
             {
                 UpdateImage(trackedImage);
 
-                // 업데이트 시에는 재생하지 않음
+                if (trackedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Limited)
+                {
+                    DisableImage(trackedImage);
+                }
             }
         }
 
@@ -160,15 +148,10 @@ public class ARwithAudio : MonoBehaviour
             if (trackedImage != null)
             {
                 DisableImage(trackedImage);
-
-                // 제거 시 리스트에서 삭제 (재추적 시 다시 재생 가능)
-                playedAudioSet.Remove(trackedImage.referenceImage.name);
             }
         }
     }
 
-
-    // 이미지에 해당하는 오브젝트의 위치와 회전을 업데이트하고 오브젝트 활성화
     private void UpdateImage(ARTrackedImage trackedImage)
     {
         string name = trackedImage.referenceImage.name;
@@ -179,22 +162,19 @@ public class ARwithAudio : MonoBehaviour
             tObj.transform.position = trackedImage.transform.position;
             tObj.SetActive(true);
 
-            if (_objectDescriptions.ContainsKey(name))
-            {
-                string description = _objectDescriptions[name];
-                objectDescriptionText.text = description;
-                objectDescriptionText.gameObject.SetActive(true);
-            }
+        }
+
+        if (imageToPanelMap.ContainsKey(name))
+        {
+            GameObject panel = imageToPanelMap[name];
+            panel.SetActive(true); // 패널 활성화
         }
         else
         {
-            Debug.LogWarning("No prefab found for tracked image: " + name);
+            Debug.LogWarning("'No prefab found for tracked image' or 'No panel found for tracked image': " + name);
         }
     }
 
-
-
-    // 이미지 추적 중지 시 해당 오브젝트 비활성화
     private void DisableImage(ARTrackedImage trackedImage)
     {
         string name = trackedImage.referenceImage.name;
@@ -203,74 +183,16 @@ public class ARwithAudio : MonoBehaviour
         {
             GameObject tObj = _prefabDic[name];
             tObj.SetActive(false);
+        }
 
-            // 이미지 추적이 중지될 때마다 음원 재생 상태 초기화
-            isAudioPlayed = false;
+        if (imageToPanelMap.ContainsKey(name))
+        {
+            GameObject panel = imageToPanelMap[name];
+            panel.SetActive(false); // 패널 비활성화
         }
         else
         {
-            Debug.LogWarning("No prefab found for tracked image to disable: " + name);
+            Debug.LogWarning("'No prefab found for tracked image' or 'No panel found for tracked image': " + name);
         }
     }
-
-
-    // 이미지에 해당하는 음원 재생
-    public List<AudioClip> audioList = new List<AudioClip>(); // 오디오 클립 리스트
-
-    private bool isAudioPlayed = false; // 음원이 이미 재생되었는지 여부를 추적하는 변수
-
-    // 이미지를 기준으로 음원 재생
-    private void PlayAudio(string imageName)
-    {
-        // 리스트에서 음원 찾기 (이미지 이름을 기준으로 매칭)
-        int audioIndex = GetAudioIndexForImage(imageName);
-
-        if (audioIndex >= 0 && audioIndex < audioList.Count)
-        {
-            AudioClip clip = audioList[audioIndex];
-
-            // 음원이 재생 중인지 확인
-            if (audioSource.isPlaying)
-            {
-                Debug.Log("오디오가 재생중입니다");
-                return;  // 음원이 이미 재생 중이면 재생을 중지하고 반환
-            }
-
-            audioSource.clip = clip;
-            audioSource.Play();
-            isAudioPlayed = true;  // 음원이 재생되었음을 표시
-        }
-        else
-        {
-            Debug.LogWarning("이 이미지에 해당하는 음원이 없습니다: " + imageName);
-        }
-    }
-
-    private int GetAudioIndexForImage(string imageName)
-    {
-        // 이미지 이름에 맞는 음원 인덱스를 반환하는 매핑 로직
-        Dictionary<string, int> imageAudioIndexMap = new Dictionary<string, int>()
-    {
-        { "gyojeon", 0 },
-        { "deungpae", 1 },
-        { "ssanggeom", 2 },
-        { "ssangsudo", 3 },
-        { "nangseon", 4 },
-        { "dangpa", 5 },
-        { "woldo", 6 },
-        { "hyeopdo", 7 },
-        { "gwonbeop", 8 }
-    };
-
-        // 이미지 이름이 딕셔너리에 있는지 확인하고, 있으면 해당 인덱스를 반환
-        if (imageAudioIndexMap.ContainsKey(imageName))
-        {
-            return imageAudioIndexMap[imageName];
-        }
-        else
-        {
-            return -1;  // 해당하는 음원이 없으면 -1을 반환
-        }
-    }
-
 }
